f059017b4e3f188951377827881e7715c4ac3758
Alena Alioshyna <elenakovalski1995@yandex.ru>
2026-02-10 21:17:11 +0300

    add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import {Routing} from "@/common/routing/Routing.tsx";
+import {Header} from "@/common/components/Header/Header.tsx";
 
 function App() {
 
   return (
     <>
+      <Header />
       <Routing />
     </>
   )
