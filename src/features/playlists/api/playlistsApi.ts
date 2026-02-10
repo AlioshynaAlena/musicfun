544d94bc91201e75ffc50dfb59348ff312dc5154
Alena Alioshyna <elenakovalski1995@yandex.ru>
2026-02-10 22:12:26 +0300

    usage 'useFetchPlaylistsQuery'

---
+++ src/features/playlists/api/playlistsApi.ts
@@ -0,0 +1,30 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import type {
+  PlaylistsResponse
+} from "@/features/playlists/api/playlistsApi.types.ts";
+
+
+export const playlistsApi = createApi({
+  reducerPath: 'playlistsApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_BASE_URL,
+    headers: {
+      'API-KEY': import.meta.env.VITE_API_KEY,
+    },
+  }),
+  endpoints: build => ({
+    // Типизация аргументов (<возвращаемый тип, тип query аргументов (`QueryArg`)>)
+    // `query` по умолчанию создает запрос `get` и указание метода необязательно
+    fetchPlaylists: build.query<PlaylistsResponse, void>({
+      query: () => {
+        return {
+          method: 'get',
+          url: `playlists`,
+        }
+      },
+    }),
+  }),
+})
+
+
+export const { useFetchPlaylistsQuery } = playlistsApi
